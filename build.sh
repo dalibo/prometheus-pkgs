f90724f23a4053606556f6831743ba11d1682649
Alexandre <alexandre.pereira@dalibo.com>
2025-03-19 14:53:16 +0100

    Update node_exporter version 0.9.0

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ build_alertmanager() {
 
 build_postgres_exporter() {
   # postgres_exporter version
-  VERSION="0.15.0"
+  VERSION="0.17.1"
   wget https://github.com/prometheus-community/postgres_exporter/releases/download/v${VERSION}/postgres_exporter-${VERSION}.linux-amd64.tar.gz -O _build/postgres_exporter-${VERSION}.tar.gz -c
 
   mkdir -p _build/postgres_exporter
@@ -44,7 +44,7 @@ build_postgres_exporter() {
 
 build_node_exporter() {
   # node_exporter version
-  VERSION="1.8.2"
+  VERSION="1.9.0"
   wget https://github.com/prometheus/node_exporter/releases/download/v${VERSION}/node_exporter-${VERSION}.linux-amd64.tar.gz -O _build/node_exporter-${VERSION}.tar.gz -c
 
   mkdir -p _build/node_exporter
