2af64bfc03411d6df57116d6509d96d61f5a31f3
arno <arnaud.bruniquel@dalibo.com>
2026-07-22 14:50:34 +0200

    Upgrade prometheus to 3.13.1

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 build_prometheus() {
   # Prometheus version
-  VERSION="3.2.1"
+  VERSION="3.13.1"
   wget https://github.com/prometheus/prometheus/releases/download/v${VERSION}/prometheus-${VERSION}.linux-amd64.tar.gz -O _build/prometheus-${VERSION}.tar.gz -c
 
   mkdir -p _build/prometheus
